87c95996d312ce4fe33bd9f1cf50a300c8525dac
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-27 22:37:49 +0500

    taking sides from user

--- Sides of a Triangle/Program.cs
+++ Sides of a Triangle/Program.cs
@@ -2,7 +2,17 @@
 {
     static void Main(string[] args)
     {
+        // prompting user to enter side1 of the triangle
+        Console.WriteLine("Enter The First Side: ");
+        double side1 = Convert.ToDouble(Console.ReadLine());
 
+        // prompting user to enter side2 of the triangle
+        Console.WriteLine("Enter The Second Side: ");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        // prompting user to enter side3 of the triangle
+        Console.WriteLine("Enter The Third Side: ");
+        double side3 = Convert.ToDouble(Console.ReadLine());
     }
     // end of main method
 }
